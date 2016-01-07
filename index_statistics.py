0d6849ae3906ac7f96832eccbb1076e348918d41
Vide Karlsson <vide.karlsson@gmail.com>
2016-01-07 16:18:43 +0100

    typo

--- index_statistics.py
+++ index_statistics.py
@@ -12,12 +12,14 @@ def print_index_statistics():
     print(str(len(word_index)))
     max_len = 0
     for word in word_index:
-        if (len(word_index[word])>max_len):
+        if  (len(word_index[word])>max_len):
             max_len = len(word_index[word])
     print('Most frequent term: '+ str(max_len))
 
     bigram_index = index.get_index(BIGRAM_INDEX_PICKLE_FILE)
     print('Number of bigram in corpus:')
-    print(str(len(word_index)))
+    print(str(len(bigram_index)))
+
+print_index_statistics()
     
 
