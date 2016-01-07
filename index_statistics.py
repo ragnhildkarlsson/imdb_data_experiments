3819aeb01e82c2ee87932f8c1998b1862f4d8a4c
Vide Karlsson <vide.karlsson@gmail.com>
2016-01-07 16:16:16 +0100

    typo

--- index_statistics.py
+++ index_statistics.py
@@ -11,8 +11,8 @@ def print_index_statistics():
     print('Number of term in corpus:')
     print(str(len(word_index)))
     max_len = 0
-    for(word in word_index):
-        if(len(word_index[word])>max_len):
+    for word in word_index:
+        if (len(word_index[word])>max_len):
             max_len = len(word_index[word])
     print('Most frequent term: '+ str(max_len))
 
