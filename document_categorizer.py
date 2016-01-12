94ab9b63a61ce5f99f7dd86319a953eb082ad92d
Vide Karlsson <vide.karlsson@gmail.com>
2016-01-12 16:03:40 +0100

    pickle big datastrctures

--- document_categorizer.py
+++ document_categorizer.py
@@ -1,6 +1,7 @@
 import nltk
 import os
 from operator import itemgetter
+import pickle
 
 
 import index
@@ -9,47 +10,79 @@ import doc_word_simularity
 import word_simularities
 
 
-TEST_DATA_FOLDER = "data/test_sub"
-TRAIN_DATA_FOLDER = "data/train_data"
+TEST_DATA_FOLDER = "data/test_data"
+TEST_DATA_ALL_CATEGORIES_PICKLE = "data/all_categories_pickle"
+TEST_DATA_TF_IDF_MAP_PICKLE = "data/tf_idf_map_pickle"
+TEST_DATA_CATEGORIZED_DOCUMENTS_PICKLE = "data/categorized_documents_pickle"
 
+TRAIN_DATA_FOLDER = "data/train_data"
 WORD_INDEX_PICKLE_FILE = "data/index_word_pickle_file"
 BIGRAM_INDEX_PICKLE_FILE = "data/index_bigram_pickle_file"
 NUMBER_OF_DOCUMENTS = 220000
 DICE_WEIGHT_FILTER_LIMIT = 0.05
 DICE_WORD_FREQUENCY_LIMIT = 0.04
 
+TEST_DATA_CATEGORY_HIEARACHY = {'religion':{'christianity','buddhism','hinduism','islam','judaism'},'christianity':{'christmas'},'sport':{'baseball','basketball','bicyckle','boxing','football','golf','hockey','skiing','soccer','surfing','swimming','tennis','wrestling','horseracing','olympic_games'},\
+                                'water_sport':{'surfing','swimming'}, 'motoring':{'motorcycle','car'}, 'nature':{'animal'},'art':{'cinema','theater','music','opera','classical_music', 'jazz','pop', 'country_music','hip hop', 'dance'}, 'music':{'opera','classical_music','jazz','pop', 'country_music','hip_hop'},\
+                                'science':{'medicine','technology','pyscology'},'medicine':{'disability'}, 'education':{'school','collage'}, 'crime':{'prision','mafia','drugs','fraud','gambling','terroism'}}
 
 
-def get_all_test_files_list(test_folder):
+
+def load_test_data_pickle(file_path):
+    resourse = pickle.load( open(file_path, "rb" ) )
+    return resourse
+
+def print_test_data_pickle(resourse, file_path):
+    pickle.dump(resourse, open(file_path,'wb'))
+
+
+def create_test_data_set(test_folder):
     sub_directories = document_processer.get_names_of_subdirectory_list(test_folder)
-    test_document_paths = []
+    document_names = set()
+    document_texts = {}
+    doc_category_map = {}
+    all_categories = []
     for subdirectory in sub_directories:
-        subdir_path = os.path.join(test_folder, subdirectory)
-        docs = document_processer.get_names_of_files_in_directory(subdir_path)
-        test_document_paths = test_document_paths + [os.path.join(subdir_path,doc) for doc in docs]
-    return test_document_paths
+        category = subdirectory
+        all_categories.append(category)
+        category_path = os.path.join(test_folder, subdirectory)
+        all_docs = document_processer.get_names_of_files_in_directory(category_path)
+        doc_category_map[category]=[]
+        for doc in all_docs:
+            doc_category_map[category].append(doc)
+        
+        new_docs = [doc for doc in all_docs if not doc in document_names]
+        empty_string = ''
+        for doc in new_docs:
+            document_names.add(doc)
+            doc_path = os.path.join(category_path,doc)
 
-def get_docs_id_tfidf_map(test_files_path, word_index, bigram_index, n_docs):
+            document_text = document_processer.get_document_string(doc_path, empty_string)
+            document_texts[doc] = document_text
+
+    return all_categories, document_texts, doc_category_map
+
+
+def create_docs_id_tfidf_map(document_texts, word_index, bigram_index, n_docs):
     max_freq = 0
     for word in word_index:
         if(len(word_index[word])>max_freq):
             max_freq = len(word_index[word])
     doc_tfidf_map = {}
     # word_tf_idf_values
-    for doc_path in test_files_path:
-        empty_string = ''
-        document = document_processer.get_document_string(doc_path, empty_string)
+    for doc in document_texts:
+        document = document_texts[doc]
         document = document_processer.preprocess_document(document)
-        doc_tfidf_map[doc_path] = doc_word_simularity.get_tf_idf_map(document, max_freq, n_docs, word_index)
+        doc_tfidf_map[doc] = doc_word_simularity.get_tf_idf_map(document, max_freq, n_docs, word_index)
 
-    for doc_path in test_files_path:
-        document = document_processer.get_document_string(doc_path, empty_string)
+    for doc in document_texts:
+        document = document_texts[doc]
         document = document_processer.preprocess_document(document)
         bigram_freqDist = nltk.FreqDist(nltk.bigrams(document))
         document = list(bigram_freqDist.keys())
         document = [document_processer.bigram_to_string(bigram) for bigram in document ]
         bigram_tfidf = doc_word_simularity.get_tf_idf_map(document, max_freq, n_docs, bigram_index)
-        doc_tfidf_map[doc_path].update(bigram_tfidf)
+        doc_tfidf_map[doc].update(bigram_tfidf)
 
     return doc_tfidf_map
 
@@ -73,19 +106,32 @@ def get_ranked_documents(category, tfidf_map, n_docs, reference_words, context_w
     return ranked_documents
 
 
-test_docs = get_all_test_files_list(TEST_DATA_FOLDER)
-print(test_docs)
+
+def dice_based_categorisation(test_categories, categorized_docs, document_texts, word_index, bigram_index, category_hierarchy, n_docs):
+    tfidf_map = get_docs_id_tfidf_map(document_texts, word_index, bigram_index, n_docs)
+
+
+
+all_categories, document_texts, doc_category_map = create_test_data_set(TEST_DATA_FOLDER)
+print_test_data_pickle(doc_category_map, TEST_DATA_CATEGORIZED_DOCUMENTS_PICKLE)
+print_test_data_pickle(all_categories, TEST_DATA_ALL_CATEGORIES_PICKLE)
+
 word_index = index.get_index(WORD_INDEX_PICKLE_FILE)
 bigram_index = index.get_index(BIGRAM_INDEX_PICKLE_FILE)
 print('loaded index')
-tfidf_map = get_docs_id_tfidf_map(test_docs,word_index,bigram_index,NUMBER_OF_DOCUMENTS)
-category = "airplane"
-category_posting_list = word_index[category]
-reference_words, context_words = word_simularities.get_dice_based_key_words(word_index, bigram_index, TRAIN_DATA_FOLDER, category_posting_list, DICE_WEIGHT_FILTER_LIMIT, DICE_WORD_FREQUENCY_LIMIT, NUMBER_OF_DOCUMENTS)
-print(reference_words)
-print(context_words)
-reference_words = set([reference[0] for reference in reference_words])
-context_words = set([context_word[0] for context_word in context_words])
+tfidf_map = create_docs_id_tfidf_map(document_texts,word_index, bigram_index, NUMBER_OF_DOCUMENTS)
+print('created tfidf_map')
+print_test_data_pickle(tfidf_map, TEST_DATA_TF_IDF_MAP_PICKLE)
 
-ranked_docs = get_ranked_documents(category, tfidf_map, NUMBER_OF_DOCUMENTS, reference_words,context_words)
-print(ranked_docs)
+
+
+
+# category = "airplane"
+# category_posting_list = word_index[category]
+# reference_words, context_words = word_simularities.get_dice_based_key_words(word_index, bigram_index, TRAIN_DATA_FOLDER, category_posting_list, DICE_WEIGHT_FILTER_LIMIT, DICE_WORD_FREQUENCY_LIMIT, NUMBER_OF_DOCUMENTS)
+# print(reference_words)
+# print(context_words)
+# reference_words = set([reference[0] for reference in reference_words])
+# context_words = set([context_word[0] for context_word in context_words])
+# ranked_docs = get_ranked_documents(category, tfidf_map, NUMBER_OF_DOCUMENTS, reference_words,context_words)
+# print(ranked_docs)
