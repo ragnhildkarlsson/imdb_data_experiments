ecda9f95f8e26013a10aa4bfd04fb46a866069fc
Vide Karlsson <vide.karlsson@gmail.com>
2016-01-26 16:29:25 +0100

    seperate_keyword_filtering

---
+++ precalculate_test_data_resourses.py
@@ -0,0 +1,93 @@
+pickle_handler
+
+TEST_DATA_FOLDER = "data/test_data"
+TEST_DATA_GOLD_STANDARD_CATEGORIZATION = "data/test_data_pickles/gold_standard_categorization_pickle"
+TEST_DATA_ALL_CATEGORIES_LIST = "data/test_data_pickles/all_categories_list"
+TEST_DATA_TF_IDF_MAP = "data/test_data_pickles/tf_idf_map_pickle"
+TEST_DATA_DICE_BASED_KEYWORD_RANKING = "data/test_data_pickles/dice_based_keyword_rankning"
+
+WORD_INDEX_PICKLE_FILE = "data/index/index_word_pickle_file"
+BIGRAM_INDEX_PICKLE_FILE = "data/index/index_bigram_pickle_file"
+NUMBER_OF_DOCUMENTS = 220000
+
+def create_test_data_set(test_folder):
+    sub_directories = document_processer.get_names_of_subdirectory_list(test_folder)
+    document_names = set()
+    document_texts = {}
+    gold_standard_categorization = {}
+    all_categories = []
+    for subdirectory in sub_directories:
+        category = subdirectory
+        if not category == 'no_category': 
+            all_categories.append(category)
+        category_path = os.path.join(test_folder, subdirectory)
+        all_docs = document_processer.get_names_of_files_in_directory(category_path)
+        gold_standard_categorization[category]=[]
+        for doc in all_docs:
+            gold_standard_categorization[category].append(doc)
+        
+        new_docs = [doc for doc in all_docs if not doc in document_names]
+        empty_string = ''
+        for doc in new_docs:
+            document_names.add(doc)
+            doc_path = os.path.join(category_path,doc)
+
+            document_text = document_processer.get_document_string(doc_path, empty_string)
+            document_texts[doc] = document_text
+
+    return all_categories, document_texts, gold_standard_categorization
+
+
+def create_docs_id_tf_idf_map(document_texts, word_index, bigram_index, n_docs):
+    max_freq = 0
+    for word in word_index:
+        if(len(word_index[word])>max_freq):
+            max_freq = len(word_index[word])
+    doc_tf_idf_map = {}
+    # word_tf_idf_values
+    for doc in document_texts:
+        document = document_texts[doc]
+        document = document_processer.preprocess_document(document)
+        doc_tf_idf_map[doc] = doc_word_simularity.get_tf_idf_map(document, max_freq, n_docs, word_index)
+
+    for doc in document_texts:
+        document = document_texts[doc]
+        document = document_processer.preprocess_document(document)
+        bigram_freqDist = nltk.FreqDist(nltk.bigrams(document))
+        document = list(bigram_freqDist.keys())
+        document = [document_processer.bigram_to_string(bigram) for bigram in document ]
+        bigram_tf_idf = doc_word_simularity.get_tf_idf_map(document, max_freq, n_docs, bigram_index)
+        doc_tf_idf_map[doc].update(bigram_tf_idf)
+
+    return doc_tf_idf_map
+
+
+def create_dice_ranked_keywords(n,categories, word_index, bigram_index, train_data_folder):
+    reference_words = {}
+    context_words = {}
+    for category in categories:
+        dice_ranked_keywords = []
+        if category not in word_index and category not in bigram_index:
+            print('WARNING: Category not in index: '+ category)
+        else:
+            if '_' in category:
+                category_posting_list = bigram_index[category]
+                dice_ranked_keywords = word_simularities.get_n_dice_based_key_words(n, word_index, bigram_index, train_data_folder, category_posting_list)
+            else:
+                category_posting_list = word_index[category]
+                r,c = word_simularities.get_dice_based_key_words(word_index, bigram_index, train_data_folder, category_posting_list, DICE_WEIGHT_FILTER_LIMIT, DICE_WORD_FREQUENCY_LIMIT, NUMBER_OF_DOCUMENTS)
+
+        #ensure category name have high rank
+        dice_ranked_keywords = [ranked_word for ranked_word in dice_ranked_keywords if ranked_word[0] != category]
+        dice_ranked_keywords.append((category,1.0))
+    return dice_ranked_keywords
+
+def create gavagai_ranked_keywords():
+    #TODO
+    pass
+
+# CREATE PROCEDURE
+all_categories, document_texts, gold_standard_categorization  = create_test_data_set(TEST_DATA_FOLDER)
+pickle_handler.print_pickle(all_categories, TEST_DATA_ALL_CATEGORIES_LIST)
+pickle_handler.print_pickle(gold_standard_categorization,TEST_DATA_GOLD_STANDARD_CATEGORIZATION)
+
--- word_simularities.py
+++ word_simularities.py
@@ -6,10 +6,8 @@ import document_processer
 
 ROOT_DATA_FOLDER = "data"
 TRAIN_DATA_FOLDER = "data/train_data"
-FILENAME_TRAIN_DATA_SUBSET = "list_train_data_subset"
-WORD_INDEX_PICKLE_FILE = "data/index_word_pickle_file"
-BIGRAM_INDEX_PICKLE_FILE = "data/index_bigram_pickle_file"
-NUMBER_OF_DOCUMENTS = 220000
+WORD_INDEX_PICKLE_FILE = "data/index/index_word_pickle_file"
+BIGRAM_INDEX_PICKLE_FILE = "data/index/index_bigram_pickle_file"
 
 def get_list_of_categories(test_data_folder):
     return next(os.walk(test_data_folder))[1]
@@ -19,10 +17,11 @@ def calculate_dice_coefficients_word(category_posting_list, word_index, train_da
     dice_coefficents = {}
     print("Number of docs")
     print(len(category_posting_list))
+    
     calculated_posts = 0
     for post in category_posting_list:
-        doc_file_mame = str(post[0])
-        document = document_processer.get_document_string(doc_file_mame, train_data_folder)
+        doc_file_name = str(post[0])
+        document = document_processer.get_document_string(doc_file_name, train_data_folder)
         document = document_processer.preprocess_document(document)
         for word in document:
             if word not in dice_coefficents and word in word_index:
@@ -30,7 +29,7 @@ def calculate_dice_coefficients_word(category_posting_list, word_index, train_da
                 intersection = index.intersection_search(category_posting_list, word_posting_list)
                 dice_coefficents[word] = len(intersection)/(len(word_posting_list)+len(category_posting_list))
         calculated_posts +=1
-        if(calculated_posts%10 ==0):
+        if(calculated_posts%50==0):
             print(calculated_posts)
     return dice_coefficents
 
@@ -38,8 +37,8 @@ def calculate_dice_coefficients_bigram(category_posting_list, bigram_index, trai
     dice_coefficents = {}
     calculated_posts = 0
     for post in category_posting_list:
-        doc_file_mame = str(post[0])
-        document = document_processer.get_document_string(doc_file_mame,train_data_folder)
+        doc_file_name = str(post[0])
+        document = document_processer.get_document_string(doc_file_name,train_data_folder)
         document = document_processer.preprocess_document(document)    
         bigram_freqDist = nltk.FreqDist(nltk.bigrams(document))
         for bigram in bigram_freqDist:
@@ -49,42 +48,21 @@ def calculate_dice_coefficients_bigram(category_posting_list, bigram_index, trai
                 intersection = index.intersection_search(category_posting_list, bigram_posting_list)
                 dice_coefficents[bigram_string] = len(intersection)/(len(bigram_posting_list)+len(category_posting_list))
         calculated_posts +=1
-        if(calculated_posts%10 ==0):
+        if(calculated_posts%50==0):
             print(calculated_posts)
     return dice_coefficents
 
-def too_high_doc_frequency(word, frequency_limit, n_docs_in_corpus, index):
-    n_docs = len(index[word])
-    return frequency_limit < (n_docs / n_docs_in_corpus)
-
-def calculate_top_100_neigbours(word_simularity_list):
+def calculate_top_n_neigbours(n, word_simularity_list):
     dice_list = sorted(word_simularity_list, key=itemgetter(1), reverse=True)
-    return dice_list[:100]
+    return dice_list[:n]
 
-def get_dice_based_key_words(word_index, bigram_index, train_data_folder, category_posting_list, weight_filter_limit, frequency_limit, n_docs_in_corpus):
+def get_n_dice_based_key_words(n, word_index, bigram_index, train_data_folder, category_posting_list, weight_filter_limit, frequency_limit, n_docs_in_corpus):
     dice_coefficents_word = calculate_dice_coefficients_word(category_posting_list, word_index, train_data_folder)
-    top_100_dice_coefficients_word = calculate_top_100_neigbours(dice_coefficents_word.items())
-    top_dice_coefficients_word = [w for w in top_100_dice_coefficients_word if not too_high_doc_frequency(w[0], frequency_limit,n_docs_in_corpus,word_index)]
-    
-    dice_coefficents_bigram = calculate_dice_coefficients_bigram(category_posting_list, bigram_index,train_data_folder)
-    top_100_dice_coefficients_bigram = calculate_top_100_neigbours(dice_coefficents_bigram.items())
-    top_dice_coefficients_bigram = [ b for b in top_100_dice_coefficients_bigram if not too_high_doc_frequency(b[0], frequency_limit,n_docs_in_corpus, bigram_index)]
+    top_dice_coefficients_word = calculate_top_n_neigbours(n, dice_coefficents_word.items())
+    dice_coefficents_bigram = calculate_dice_coefficients_bigram(category_posting_list, bigram_index, train_data_folder)
+    top_dice_coefficients_bigram = calculate_top_n_neigbours(dice_coefficents_bigram.items())
+    top_n_dice_keywords = calculate_top_n_neigbours(n, top_dice_coefficients_word + top_dice_coefficients_bigram) 
+    return top_n_dice_keywords
 
-    all_dice_coefficients = calculate_top_100_neigbours(top_dice_coefficients_word + top_dice_coefficients_bigram) 
-
-    reference_words = [d for d in all_dice_coefficients if weight_filter_limit <= d[1]]
-    
-    context_words = [d for d in all_dice_coefficients if weight_filter_limit > d[1]]
-
-    return reference_words, context_words
-
-
-# word_index = index.get_index(WORD_INDEX_PICKLE_FILE)
-# category_posting_list = word_index['karate']
-# bigram_index = index.get_index(BIGRAM_INDEX_PICKLE_FILE)
-# print ('loaded index')
-# r, c = get_dice_based_key_words(word_index, bigram_index, TRAIN_DATA_FOLDER,category_posting_list, 0.05,0.04,NUMBER_OF_DOCUMENTS)
-# print(r)
-# print(c)
 
 
